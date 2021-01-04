33d782df75efe30134198ac63a90c8ad76689fe1
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2021-01-03 23:42:30 -0300

    add tests

--- noxfile.py
+++ noxfile.py
@@ -4,20 +4,21 @@ import tempfile
 import nox
 
 locations = "potosi", "tests", "noxfile.py"
-nox.options.sessions = "lint", "safety", "tests"
+nox.options.sessions = "lint", "safety", "mypy", "tests"
 
 
-@nox.session(python=["3.9", "3.8"])
+# @nox.session(python=["3.9", "3.8"])
+@nox.session(python=["3.9"])
 def tests(session):
     args = session.posargs or ["--cov", "-m", "not e2e"]
     session.run("poetry", "install", "--no-dev", external=True)
     install_with_constraints(
-        session, "coverage -E toml", "pytest", "pytest-cov", "pytest-mock"
+        session, "coverage[toml]", "pytest", "pytest-cov", "pytest-mock"
     )
     session.run("pytest", *args)
 
 
-@nox.session(python=["3.9", "3.8"])
+@nox.session(python=["3.9"])
 def lint(session):
     args = session.posargs or locations
     install_with_constraints(
@@ -52,6 +53,22 @@ def safety(session):
         )
         install_with_constraints(session, "safety")
         session.run("safety", "check", f"--file={requirements.name}", "--full-report")
+
+
+@nox.session(python=["3.9"])
+def mypy(session):
+    args = session.posargs or locations
+    install_with_constraints(session, "mypy")
+    session.run("mypy", *args)
+
+
+# @nox.session(python="3.7")
+# def pytype(session):
+#     """Run the static type checker."""
+#     args = session.posargs or ["--disable=import-error", *locations]
+#     install_with_constraints(session, "pytype")
+#     session.run("pytype", *args)
+#
 
 
 def install_with_constraints(session, *args, **kwargs):
---
+++ tests/test_parser_signal.py
@@ -0,0 +1,79 @@
+import pytest
+
+from potosi.parser_signal import ParserSignal
+
+
+@pytest.fixture()
+def signal():
+    return """ℹ️ Update New Signal Created
+〽️ #BTCUSDT
+💹 BinanceFutures (Long X5)
+
+➡️ Pending State
+💰 18742.84
+
+🔀 Entry Zone 🔀
+1️⃣ 18700
+2️⃣ 18600
+2️⃣ 18500
+
+🔆 Exit Targets:🔆
+☑️ 18800 / 100%
+
+⛔️ StopLoss ⛔️
+➡️ 18450 / 4.07% (Breakeven)
+✳️ Breakeven after 2 target
+🎇Risk 5/10
+https://www.tradingview.com/x/LsOb112c
+"""
+
+
+def test_parser_signal_created(signal):
+    parser = ParserSignal()
+    order = parser.parser_signal_created(signal)
+
+    assert order == {
+        "symbol": "BTCUSDT",
+        "market": "BinanceFutures",
+        "position_side": "Long",
+        "leverage": 5,
+        "entry_zone": ["18700", "18600", "18500"],
+        "exit_targets": ["18800"],
+        "stoploss": "18450",
+    }
+
+
+def test_parse_signal_close():
+    signal = """ℹ️ #TRXUSDT Signal Closed
+
+〽️ BinanceFutures With 3x Leverage
+
+💰 Total of 1\\8 target hit
+Profit  13.95%"""
+
+    signal2 = "ℹ️ Update: Signal LTCUSDT Closed"
+
+    parser = ParserSignal()
+    result = parser.parser_signal_closed(signal)
+    result2 = parser.parser_signal_closed(signal2)
+
+    assert result == "TRXUSDT"
+    assert result2 == "LTCUSDT"
+
+
+def test_parser_stoploss_updated():
+    signal = """⚠️ #BNBUSDT Stoploss Updated
+
+💰 Previous Stop Price - 29.13
+💰 Updated Stop Price - 30.3
+
+Please adjust your trade to match the change"""
+
+    parser = ParserSignal()
+    result = parser.parser_stoploss_updated(signal)
+
+    assert result == {
+        "symbol": "BNBUSDT",
+        "previous_stop_price": "29.13",
+        "updated_stop_price": "30.3",
+    }
