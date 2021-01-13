81c3b03dd2977edc473a2ee3b14ad3174f7eda0d
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2021-01-13 13:44:27 -0300

    fixing deploy

--- ec2/scripts/start-service.sh
+++ ec2/scripts/start-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 
-#sudo systemctl start user-stream
-#sudo systemctl start signal-receiver
+sudo systemctl start user-stream
+sudo systemctl start signal-receiver
--- ec2/scripts/stop-service.sh
+++ ec2/scripts/stop-service.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 source /home/ec2-user/.bash_profile
 
-#sudo systemctl stop user-stream
-#sudo systemctl stop signal-receiver
+sudo systemctl stop user-stream
+sudo systemctl stop signal-receiver
