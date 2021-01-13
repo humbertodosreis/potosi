b2370dfe6346a4133495a6d15aca0fa444343a9a
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2021-01-13 15:58:31 -0300

    fixing after install script

--- ec2/scripts/install_dependencies.sh
+++ ec2/scripts/install_dependencies.sh
@@ -10,5 +10,6 @@ sudo systemctl enable user-stream
 sudo systemctl enable signal-receiver
 
 poetry env use 3.9
-poetry shell
+# shellcheck disable=SC1090
+source $(poetry env info --path)/bin/activate
 poetry install --no-dev
