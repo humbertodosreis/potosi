67fe3d4835a3f645c794c69dc7e7ae81f24671d6
Humberto dos Reis Rodrigues <rrodrigues.humberto@gmail.com>
2021-01-15 01:57:38 -0300

    fixing installer

---
+++ ec2/scripts/after_install.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -xe
+function get_parameter () {
+  local param="aws ssm get-parameter "
+
+  if [ "$1" == "db_password" ]
+  then
+    param+=" --with-decryption"
+  fi
+
+  param+=" --name /potosi/$1 --query Parameter.Value"
+
+  local param_value
+  param_value=$(eval "$param")
+  echo "$param_value" | sed -e 's/^"//' -e 's/"$//'
+}
+
+db_name=$(get_parameter db_name)
+db_username=$(get_parameter db_username)
+db_password=$(get_parameter db_password)
+db_host=$(get_parameter db_host)
+telegram_api_id=$(get_parameter telegram_api_id)
+telegram_api_hash=$(get_parameter telegram_api_hash)
+binance_api_key=$(get_parameter binance_api_key)
+binance_api_secret=$(get_parameter binance_api_secret)
+user_input_channel=$(get_parameter user_input_channel)
+
+cat > /home/ec2-user/app/release/.env << EOF
+APP_ENV=production
+APP_TELEGRAM_API_ID=$telegram_api_id
+APP_TELEGRAM_API_HASH=$telegram_api_hash
+APP_BINANCE_API_KEY=$binance_api_key
+APP_BINANCE_API_SECRET=$binance_api_secret
+APP_USER_INPUT_CHANNEL=$user_input_channel
+DB_USER=$db_username
+DB_PASSWORD=$db_password
+DB_NAME=$db_name
+DB_HOST=$db_host
+DB_PORT=5432
+EOF
+
+sudo cp ec2/systemd/*.service /etc/systemd/system/
+sudo systemctl daemon-reload
+
+sudo systemctl enable user-stream
+sudo systemctl enable signal-receiver
